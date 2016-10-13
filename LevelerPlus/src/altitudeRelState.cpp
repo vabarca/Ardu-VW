aef464d3f86bf99ad293ec4edccfdd95595b720e
vabarca <viabfer@gmail.com>
2016-10-14 00:32:51 +0200

    Add new states

---
+++ LevelerPlus/src/altitudeRelState.cpp
@@ -0,0 +1,60 @@
+//-----------------------------------------------------------------------------
+//-----------------------------------------------------------------------------
+
+/** @file  altitudeRelState.cpp
+ *  @date  July, 2016
+ *  @brief Altitude State class
+ *
+ *
+ *  @author Cooked by Vicente A. (TT)
+ *  @bug No know bugs.
+ */
+ //-----------------------------------------------------------------------------
+ //-----------------------------------------------------------------------------
+
+#include "includes.h"
+
+//-----------------------------------------------------------------------------
+//-----------------------------------------------------------------------------
+
+void CAltitudeRelState::button0ShortPress()
+{
+  _pStateMachine->setState(_pStateMachine->getPressureState());
+}
+
+//-----------------------------------------------------------------------------
+
+void CAltitudeRelState::button1ShortPress()
+{
+  _pStateMachine->setState(_pStateMachine->getAltitudeState());
+}
+
+//-----------------------------------------------------------------------------
+
+void CAltitudeRelState::button0LongPress()
+{
+  _pStateMachine->_saveAltitudeRef(_pStateMachine->_fAltitude);
+  _pStateMachine->_fAltitudeRef = _pStateMachine->_fAltitude;
+}
+
+//-----------------------------------------------------------------------------
+
+void CAltitudeRelState::button1LongPress()
+{
+  _pStateMachine->_saveAltitudeRef(_pStateMachine->_fAltitude);
+  _pStateMachine->_fAltitudeRef = _pStateMachine->_fAltitude;
+}
+
+//-----------------------------------------------------------------------------
+
+void CAltitudeRelState::drawCurrentState()
+{
+  _pStateMachine->_u8g.setPrintPos(15,35);
+  _pStateMachine->_u8g.print(_pStateMachine->_fAltitude -
+    _pStateMachine->_fAltitudeRef, 1);
+  _pStateMachine->_u8g.setPrintPos(85,35);
+  _pStateMachine->_u8g.print("m rel");
+}
+
+//-----------------------------------------------------------------------------
+//-----------------------------------------------------------------------------
---
+++ LevelerPlus/src/altitudeRelState.h
@@ -0,0 +1,44 @@
+//-----------------------------------------------------------------------------
+//-----------------------------------------------------------------------------
+
+/** @file  altitudeRelState.h
+ *  @date  July, 2016
+ *  @brief Relative altitude State class
+ *
+ *
+ *  @author Cooked by Vicente A. (TT)
+ *  @bug No know bugs.
+ */
+ //-----------------------------------------------------------------------------
+ //-----------------------------------------------------------------------------
+
+#ifndef __ALTITUDE_REL_STATE_H__
+ #define __ALTITUDE_REL_STATE_H__
+
+ //-----------------------------------------------------------------------------
+ //-----------------------------------------------------------------------------
+
+ #include "stateMachine.h"
+
+ //-----------------------------------------------------------------------------
+
+class CAltitudeRelState : public IState{
+protected:
+	CStateMachine* _pStateMachine;
+public:
+	CAltitudeRelState(CStateMachine* pStateMachine):_pStateMachine(pStateMachine){}
+	~CAltitudeRelState(){}
+	void button0ShortPress();
+	void button1ShortPress();
+	void button0LongPress();
+	void button1LongPress();
+	void drawCurrentState();
+};
+
+//-----------------------------------------------------------------------------
+//-----------------------------------------------------------------------------
+
+#endif  // __ALTITUDE_REL_STATE_H__
+
+//-----------------------------------------------------------------------------
+//-----------------------------------------------------------------------------
