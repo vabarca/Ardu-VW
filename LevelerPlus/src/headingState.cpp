6bcb7ccf73f8fafa47938407320124d4c90c9d0e
vabarca <viabfer@gmail.com>
2017-01-11 21:35:55 +0100

    Update project

--- LevelerPlus/src/headingState.cpp
+++ LevelerPlus/src/headingState.cpp
@@ -41,25 +41,21 @@ void CHeadingState::drawCurrentState() {
   // THIS LINE MUST BE UNCOMMENTED IF THIS IS THE DEFAULT STATE
   //_pStateMachine->_ulTimeStamp = millis();
 
-  const uint8_t ui8CircleCentre(97);
+  const uint8_t ui8CircleCentre(95);
 
-  _pStateMachine->_u8g.drawCircle(ui8CircleCentre, 32, 31);
+  uint8_t xN((uint8_t)(31 * sin(/*M_PI_D -*/ _pStateMachine->_fHeading)));
+  uint8_t yN((uint8_t)(31 * cos(/*M_PI_D -*/ _pStateMachine->_fHeading)));
 
-  uint8_t xN((uint8_t)(31 * sin(M_PI_D - _pStateMachine->_fHeading)));
-  uint8_t yN((uint8_t)(31 * cos(M_PI_D - _pStateMachine->_fHeading)));
-  _pStateMachine->_u8g.setFontPosCenter();
-  _pStateMachine->_u8g.drawStr(xN + ui8CircleCentre,
-                               -yN + SSD1306_LCDHEIGHT_MED, "N");
-  /*_pStateMachine->_u8g.drawStr(-xN + SSD1306_LCDWIDTH_MED,
-                               yN + SSD1306_LCDHEIGHT_MED, "S");
-  _pStateMachine->_u8g.drawStr(-xN + SSD1306_LCDWIDTH_MED,
-                               -yN + SSD1306_LCDHEIGHT_MED, "E");
-  _pStateMachine->_u8g.drawStr(xN + SSD1306_LCDWIDTH_MED,
-                               yN + SSD1306_LCDHEIGHT_MED, "O");*/
-  _pStateMachine->_u8g.setFontPosBaseline();
-  _pStateMachine->_u8g.setPrintPos(0, 10);
-  _pStateMachine->_u8g.print((uint16_t)(_pStateMachine->_fHeading * RAD_2_DEG),
-                             1);
+  _pStateMachine->_u8g.drawLine(
+      xN + ui8CircleCentre, -yN + SSD1306_LCDHEIGHT_MED, -xN + ui8CircleCentre,
+      yN + SSD1306_LCDHEIGHT_MED);
+  _pStateMachine->_u8g.drawCircle(ui8CircleCentre, SSD1306_LCDHEIGHT_MED, 31);
+  _pStateMachine->_setDrawNumberLines(1);
+  _pStateMachine->_u8g.setPrintPos(
+      (u8g_int_t)_pStateMachine->_getFloatDrawColPos(_pStateMachine->_fHeading),
+      (u8g_int_t)_pStateMachine->_getDrawRowPos(0));
+  _pStateMachine->_u8g.print(_pStateMachine->_fHeading * RAD_2_DEG, 0);
+  _pStateMachine->_u8g.print("º");
 }
 
 //-----------------------------------------------------------------------------
