5d67cc068100be3b145c1055d9052f6c88494673
vabarca <viabfer@gmail.com>
2023-11-03 19:57:28 +0100

    Fix static analisys bugs

--- LevelerPlus/lib/MPU60X0/MPU60X0.cpp
+++ LevelerPlus/lib/MPU60X0/MPU60X0.cpp
@@ -878,7 +878,7 @@ void MPU60X0::setMasterClockSpeed(uint8_t speed) {
  * In read mode, the result of the read is placed in the lowest available 
  * EXT_SENS_DATA register. For further information regarding the allocation of
  * read results, please refer to the EXT_SENS_DATA register description
- * (Registers 73 – 96).
+ * (Registers 73 ï¿½ 96).
  *
  * The MPU-6050 supports a total of five slaves, but Slave 4 has unique
  * characteristics, and so it has its own functions (getSlave4* and setSlave4*).
@@ -2969,7 +2969,7 @@ void MPU60X0::readMemoryBlock(uint8_t *data, uint16_t dataSize, uint8_t bank, ui
         i += chunkSize;
 
         // uint8_t automatically wraps to 0 at 256
-        address + chunkSize;
+        //address + chunkSize;
 
         // if we aren't done, update bank (if necessary) and address
         if (i < dataSize) {
